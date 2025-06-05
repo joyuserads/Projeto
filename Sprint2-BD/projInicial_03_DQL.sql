6e05f1c0716d561788f93cdb2889e5b09427e12e
Joyce Santos <joycessilva23@hotmail.com>
2025-06-05 16:49:51 -0300

    Modelagens de dados(BD) e baixa fidelidade adicionada

---
+++ Sprint2-BD/projInicial_03_DQL.sql
@@ -0,0 +1,15 @@
+USE projetoInicial3T
+GO
+
+--  Nome que o projeto deverá ter : senai_projInicial3T_03_DQL.sql
+
+
+SELECT * FROM tiposUsuario
+
+SELECT * FROM usuario
+
+SELECT * FROM salas
+
+SELECT * FROM equipamentos
+
+SELECT * FROM salasEquipamentos
